c6c6af62c8c9ee5fd19ba51d76093e2eac178a7c
Aakash <aakash19here@gmail.com>
2024-07-28 17:18:53 +0530

    chore: add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://astro-tina-cms.vercel.app",
   integrations: [mdx(), sitemap()],
 });
